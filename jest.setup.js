4eb0554ea247485522204d35253bad2794d89af4
thinkart <marcelo.jobs@gmail.com>
2024-07-19 17:59:31 -0300

    chore: Update Jest configuration and add setup file

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
 // import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
+
+// jest.config.js
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testMatch: ["**/test/*.test.js", "!**/src/**"],
+    transform: {
+        "^.+\\.js$": "babel-jest",
+    },
+};
